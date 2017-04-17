3cb20ec6329b708f59cecbe12165acadfeb28598
Nagaprasad Natarajaurs <nagaprasad@outlook.in>
2017-04-16 22:46:38 -0400

    create appropriate package, update gitignore change directory structure

---
+++ src/com/mkchaudh/nnataraj/ssingh11/detectnetworkanomaly/DetectNetworkAnomalyJob.java
@@ -0,0 +1,14 @@
+package com.mkchaudh.nnataraj.ssingh11.detectnetworkanomaly;
+
+import org.apache.hadoop.mapreduce.Job;
+
+/**
+ * Created by mehal, nagaprasad & suruchi on 4/16/17.
+ */
+public class DetectNetworkAnomalyJob {
+
+    public static void main(String[] args) throws Exception {
+        Job job = new Job();
+    }
+
+}
